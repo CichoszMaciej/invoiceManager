82c6f4643781eb007a743d990659dc535fa5eb54
Paweł Cichosz <U15428@s.tu.koszalin.pl>
2021-03-13 12:06:45 +0100

    spolszczenie formularza faktury

--- clients/models.py
+++ clients/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class Client(models.Model):
-    nip = models.CharField(max_length=30, null=True)
-    client_name = models.CharField(max_length=30, null=True)
-    address = models.CharField(max_length=64, null=True)
-    city = models.CharField(max_length=64, null=True)
+    nip = models.CharField(max_length=30, null=True, verbose_name="NIP")
+    client_name = models.CharField(max_length=30, null=True, verbose_name= "Nazwa Klienta")
+    address = models.CharField(max_length=64, null=True, verbose_name="Adres")
+    city = models.CharField(max_length=64, null=True, verbose_name="Miasto")
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- invoices/models.py
+++ invoices/models.py
@@ -20,19 +20,19 @@ class InvoiceType(models.Model):
 
 
 class Invoice(models.Model):
-    invoice_status = models.ForeignKey(InvoiceStatus, null=True, on_delete=models.DO_NOTHING)
-    invoice_type = models.ForeignKey(InvoiceType, null=True, on_delete=models.DO_NOTHING)
-    invoice_nr = models.CharField(null=True, max_length=128)
-    client = models.ForeignKey(Client, on_delete=models.DO_NOTHING)
-    price_gross = models.DecimalField(null=True, decimal_places=2, max_digits=16)
-    price_net = models.DecimalField(null=True, decimal_places=2, max_digits=16)
-    price_vat = models.DecimalField(null=True, decimal_places=2, max_digits=16)
-    sales_date = models.DateField(null=True)
-    invoice_date = models.DateField(null=True)
-    payment_date = models.DateField(null=True)
-    paid = models.DecimalField(null=True, decimal_places=2, max_digits=16)
-    month = models.IntegerField(null=True)
-    year = models.IntegerField(null=True)
+    invoice_status = models.ForeignKey(InvoiceStatus, null=True, on_delete=models.DO_NOTHING, verbose_name="Status faktury")
+    invoice_type = models.ForeignKey(InvoiceType, null=True, on_delete=models.DO_NOTHING, verbose_name="Typ faktury")
+    invoice_nr = models.CharField(null=True, max_length=128, verbose_name="Numer Faktury")
+    client = models.ForeignKey(Client, on_delete=models.DO_NOTHING, verbose_name="Klient")
+    price_gross = models.DecimalField(null=True, decimal_places=2, max_digits=16, verbose_name="Cena brutto")
+    price_net = models.DecimalField(null=True, decimal_places=2, max_digits=16, verbose_name="Cena netto")
+    price_vat = models.DecimalField(null=True, decimal_places=2, max_digits=16, verbose_name="Wartość VAT")
+    sales_date = models.DateField(null=True, verbose_name="Data sprzedaży")
+    invoice_date = models.DateField(null=True, verbose_name="Data wystawienia faktury")
+    payment_date = models.DateField(null=True, verbose_name="Data zapłaty")
+    paid = models.DecimalField(null=True, decimal_places=2, max_digits=16, verbose_name="Wartość opłacenia")
+    month = models.IntegerField(null=True, verbose_name="Miesiąc")
+    year = models.IntegerField(null=True, verbose_name="Rok")
 
     def __str__(self):
         return self.invoice_nr
