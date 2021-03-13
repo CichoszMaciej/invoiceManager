056d9cff5b8d07aaf9b08a0302a96533ac59db41
Maciej Cichosz <cichosz.maciej@gmail.com>
2021-03-13 11:53:19 +0100

    Poprawek

--- invoices/admin.py
+++ invoices/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from invoices.models import InvoiceRecord, Invoice
+from invoices.models import InvoiceRecord, Invoice, InvoiceStatus, InvoiceType
 
 admin.site.register(InvoiceRecord)
 admin.site.register(Invoice)
+admin.site.register(InvoiceStatus)
+admin.site.register(InvoiceType)
--- products/models.py
+++ products/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 
 class Product(models.Model):
-    name = models.CharField(null=True, max_length=64)
-    description = models.CharField(null=True, max_length=255)
-    price_net = models.DecimalField(null=True, decimal_places=2, max_digits=16)
-    price_gross = models.DecimalField(null=True, decimal_places=2, max_digits=16)
-    price_vat = models.DecimalField(null=True, decimal_places=2, max_digits=16)
-    vat_rate = models.IntegerField(null=True)
-    is_active = models.BooleanField(default=True)
-    quantity_stock = models.DecimalField(null=True, decimal_places=3, max_digits=16)
+    name = models.CharField(null=True, max_length=64, name='Nazwa')
+    description = models.CharField(null=True, max_length=255, name='Opis')
+    price_net = models.DecimalField(null=True, decimal_places=2, max_digits=16, name='Cena Netto')
+    price_gross = models.DecimalField(null=True, decimal_places=2, max_digits=16, name='Cena Brutto')
+    price_vat = models.DecimalField(null=True, decimal_places=2, max_digits=16, name='Wartość VAT')
+    vat_rate = models.IntegerField(null=True, name='Stawka VAT')
+    is_active = models.BooleanField(default=True, name='Czy jest aktywny?')
+    quantity_stock = models.DecimalField(null=True, decimal_places=3, max_digits=16, name='Stan magazynowy')
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
