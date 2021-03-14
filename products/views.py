e32baea74ed344aa4f24e8038452460e1c605a79
Paweł Cichosz <U15428@s.tu.koszalin.pl>
2021-03-14 16:14:11 +0100

    poprawki literówek

--- products/views.py
+++ products/views.py
@@ -28,7 +28,9 @@ def add_product(request):
         form = ProductCreate(request.POST)
         if form.is_valid():
             product = form.save(commit=False)
-            product.price_gross = product.cost * (1 + (product.gain / 100))
+            product.price_net = (float(product.cost) * (1 + (float(product.gain) / 100)))
+            product.price_gross = float(product.price_net) * (1 + (product.vat_rate.value / 100))
+            product.price_vat = product.price_gross - product.price_net
             product.save()
             return HttpResponseRedirect(reverse(product_list) + '?info=success')
         else:
